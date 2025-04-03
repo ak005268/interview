578df9ae1341e73e104f7ff6855753ae1096d87a
ak005268 <arunsrid11@gmail.com>
2025-04-03 07:24:31 +0530

    refresh issue fixed

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,7 +2,14 @@ const Dashboard=()=>{
 
     return(<>
     
-    <div className="font-bold text-6xl">Welcome to dashBoard</div>
+    <div className="font-bold text-6xl grid">
+
+<div className="w-[200px] grid-cols-2 h-[100px] bg-amber-200 rounded-2xl shadow p-5"> Hello</div>
+<div className="w-[200px]  grid-cols-2 h-[100px] bg-amber-200 rounded-2xl shadow p-5"> Hello</div>
+<div className="w-[200px]  grid-cols-1 h-[100px] bg-amber-200 rounded-2xl shadow p-5"> Hello</div>
+
+
+        </div>
 
     </>)
 
